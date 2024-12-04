9df87ef586ef25cdf88e56ef15631f7445c2e8bc
Kepin Awikwok <kcchandra@waresix.com>
2024-12-04 10:52:58 +0700

    feat(consumer): add other method of reading kafka message

--- consumer/src/consumer.go
+++ consumer/src/consumer.go
@@ -2,6 +2,7 @@ package consumer
 
 import (
 	"context"
+	"errors"
 	"golang-kafka-consumer/src/dto"
 	"golang-kafka-lib/constant"
 	"log"
@@ -9,6 +10,9 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
+// support read commited - which is for failure purpose
+// required data is the group_id should be set (it is being assumed that it was a group of consumer)
+// source: https://github.com/segmentio/kafka-go?tab=readme-ov-file#explicit-commits
 func FetchMessage(ctx context.Context, opt dto.ConsumerOpt) (err error) {
 	retryCount := 1
 	if opt.RetryTime > 1 {
@@ -62,6 +66,44 @@ func FetchMessage(ctx context.Context, opt dto.ConsumerOpt) (err error) {
 
 	if retryCount == 0 {
 		log.Println("retry count depleted, stopping programs...")
+	}
+	return
+}
+
+// this will always commit the message to the kafka, the offest will always being updated
+func ReadMessage(ctx context.Context, opt dto.ConsumerOpt) (err error) {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:               constant.GetBrokers(),
+		Topic:                 constant.KafkaTopics,
+		MaxBytes:              10e6, // 10MB
+		StartOffset:           kafka.LastOffset,
+		GroupID:               constant.KafkaTopics,
+		WatchPartitionChanges: true,
+		IsolationLevel:        kafka.ReadCommitted,
+
+		// Logger:      log.Default(),
+		ErrorLogger: log.Default(),
+	})
+
+	log.Println("start listening ...")
+	for {
+		var message kafka.Message
+		message, err = r.ReadMessage(ctx)
+		if err != nil && !errors.Is(err, context.Canceled) {
+			log.Fatal(err)
+			break
+		}
+		if errors.Is(err, context.Canceled) {
+			break
+		}
+
+		log.Println("key:", string(message.Key))
+		log.Println("value:", string(message.Value))
+		log.Println("header:", message.Headers)
+	}
+
+	if errClose := r.Close(); errClose != nil {
+		log.Fatal(errClose)
 	}
 	return
 }
